b283ddb4b3814c32ab2802a3993e8d00339f51e0
Alain Lemaire <jaxdid@gmail.com>
2017-01-19 21:06:17 +0000

    Adds WIP Rails project

---
+++ yelp/spec/features/restaurants_feature_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+feature 'restaurants' do
+  context 'no restaurants have been added' do
+    # this is a test vvvv
+    scenario 'should display a prompt to add a restaurant' do
+      visit '/restaurants'
+      expect(page).to have_content 'No restaurants yet'
+      expect(page).to have_link 'Add a restaurant'
+    end
+
+    # this is another test vvvv
+    # ...
+  end
+  
+  context 'restaurants have  been added' do
+    before do
+      Restaurant.create(name: 'KFC')
+    end
+    
+    scenario 'display restaurants' do
+      visit '/restaurants'
+      expect(page).to have_content('KFC')
+      expect(page).not_to have_content('No restaurants yet')
+    end
+  end
+end
