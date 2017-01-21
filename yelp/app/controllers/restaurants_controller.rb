68b1b1e638f6e9fd73a992b0502b1614abed9630
Alain Lemaire <jaxdid@gmail.com>
2017-01-21 23:15:47 +0000

    Add edit and delete functionality for restaurants

--- yelp/app/controllers/restaurants_controller.rb
+++ yelp/app/controllers/restaurants_controller.rb
@@ -1,23 +1,38 @@
 class RestaurantsController < ApplicationController
-  def index
-    @restaurants = Restaurant.all
+  def index # GET
+    @restaurants = Restaurant.order(id: :desc)
   end
 
-  def new
+  def new # GET
     @restaurant = Restaurant.new
   end
 
-  def create
+  def create # POST
     @restaurant = Restaurant.create(restaurant_params) # why use instance variable for POST?
     redirect_to '/restaurants'
   end
 
-  def show
+  def show # GET
     @restaurant = Restaurant.find(params[:id])
+  end
+
+  def edit # GET
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def update # PATCH / PUT
+    @restaurant = Restaurant.find(params[:id]).update(restaurant_params)
+    redirect_to '/restaurants'
+  end
+
+  def destroy
+    @restaurant = Restaurant.find(params[:id]).destroy
+    flash[:notice] = 'Restaurant deleted successfully'
+    redirect_to '/restaurants'
   end
 
   # private methods
   def restaurant_params
-    params.require(:restaurant).permit(:name)
+    params.require(:restaurant).permit(:name, :description)
   end
 end
--- yelp/spec/features/restaurants_feature_spec.rb
+++ yelp/spec/features/restaurants_feature_spec.rb
@@ -30,20 +30,52 @@ feature 'restaurants' do
       visit '/restaurants'
       click_link 'Add a restaurant'
       fill_in 'Name', with: 'KFC'
+      fill_in 'Description', with: 'Chikken'
       click_button 'Create Restaurant'
       expect(page).to have_content 'KFC'
+      expect(page).to have_content 'Chikken'
       expect(current_path).to eq '/restaurants'
     end
   end
 
   context 'viewing restaurants' do
-    let!(:bfff){ Restaurant.create(name: 'Baba\'s Fancy Food Feast') }
+    let!(:bfff){ Restaurant.create(name: 'Baba\'s Fancy Food Feast', description: 'Awesome feasts from the Far Easts') }
 
     scenario 'lets a user view a restaurant' do
       visit '/restaurants'
       click_link 'Baba\'s Fancy Food Feast'
       expect(page).to have_content 'Baba\'s Fancy Food Feast'
+      expect(page).to have_content "#{bfff.description}"
       expect(current_path).to eq "/restaurants/#{bfff.id}"
     end
-  end 
+  end
+
+  context 'editing restaurants' do
+    before { Restaurant.create name: 'KFC', description: 'Chikken', id: 1 }
+
+    scenario 'let a user edit a restaurant' do
+      visit '/restaurants'
+      click_link 'Edit KFC'
+      fill_in 'Name', with: 'Kentucky Fried Chicken'
+      fill_in 'Description', with: 'Chikkennnnn'
+      click_button 'Update Thissssssssss'
+      click_link 'Kentucky Fried Chicken'
+      expect(page).to have_content 'Kentucky Fried Chicken'
+      expect(page).to have_content 'Chikkennnnn'
+      expect(current_path).to eq '/restaurants/1'
+    end
+  end
+
+  context 'deleting restaurants' do
+    before { Restaurant.create name: 'KFC', description: 'Chikken' }
+
+    scenario 'let a user delete a restaurant' do
+      visit '/restaurants'
+      click_link 'Delete KFC'
+      expect(page).not_to have_content 'KFC'
+      expect(page).not_to have_content 'Chikken'
+      expect(page).to have_content 'Restaurant deleted successfully'
+      expect(current_path).to eq '/restaurants'
+    end
+  end
 end
